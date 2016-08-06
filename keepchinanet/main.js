dce26a7387da540f8ebe735c8a9a7c38c3dc6adf
moogle <moogle@live.cn>
2016-08-06 15:39:13 +0800

    增加账号和密码保存在localStorage

--- keepchinanet/main.js
+++ keepchinanet/main.js
@@ -1,7 +1,7 @@
 const electron = require('electron')
-// Module to control application life.
+    // Module to control application life.
 const app = electron.app
-// Module to create native browser window.
+    // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
 const ipcMain = electron.ipcMain;
@@ -13,8 +13,8 @@ const ipcMain = electron.ipcMain;
 let mainWindow
 
 global.APP_CONFIG = {
-  isDevelopment : true,
-  version: '1.0.0'
+    isDevelopment: true,
+    version: '1.0.0'
 };
 
 if (global.APP_CONFIG.isDevelopment) {
@@ -24,27 +24,27 @@ if (global.APP_CONFIG.isDevelopment) {
 }
 
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, resizable : false})
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({ width: 800, height: 600, resizable: false })
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+    // and load the index.html of the app.
+    mainWindow.loadURL(`file://${__dirname}/index.html`)
 
-  // Open the DevTools.
-  if (APP_CONFIG.isDevelopment) {
-  mainWindow.webContents.openDevTools()
-  }
+    // Open the DevTools.
+    if (APP_CONFIG.isDevelopment) {
+        mainWindow.webContents.openDevTools()
+    }
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    console.log("主窗口被关闭");
-    mainWindow = null
-    onlineStatusWindow.close();
-  })
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        console.log("主窗口被关闭");
+        mainWindow = null
+        onlineStatusWindow.close();
+    })
 }
 
 // This method will be called when Electron has finished
@@ -53,44 +53,49 @@ function createWindow () {
 let onlineStatusWindow;
 
 app.on('ready', function() {
-  console.log("app starting...");
-  createWindow();
-  onlineStatusWindow = new BrowserWindow({ width: 400, height: 300, show: true , frame : false, resizable : false});
-  if (global.APP_CONFIG.isDevelopment) {
-    onlineStatusWindow.webContents.openDevTools()
-  }
-  onlineStatusWindow.loadURL(`file://${__dirname}/online-status.html`);
+    console.log("app starting...");
+    createWindow();
+    onlineStatusWindow = new BrowserWindow({ width: 400, height: 300, show: false, frame: false, resizable: false });
+    if (global.APP_CONFIG.isDevelopment) {
+        onlineStatusWindow.webContents.openDevTools()
+    }
+    onlineStatusWindow.loadURL(`file://${__dirname}/online-status.html`);
 });
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  console.log("window-all-closed");
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== `darwin`) {
-    app.quit()
-  }
+app.on('window-all-closed', function() {
+    console.log("window-all-closed");
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== `darwin`) {
+        app.quit()
+    }
 })
 
-app.on('activate', function () {
-  console.log("app activate");
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+app.on('activate', function() {
+    console.log("app activate");
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
+/**
+ * 这个消息通知是以浏览器的navigator.onLine状态为准，实际代表网卡是否链接网络，
+ * 但是CHinaNet是需要账号登陆，所以用navigator判断是不准确的
+ */
 ipcMain.on('online-status-changed', function(event, status) {
-  console.log("收到在线/离线状态的变化通知 %s",status);
-  mainWindow.webContents.send('online-status-changed',status)
+    console.log("收到在线/离线状态的变化通知 %s", status);
 });
 
+/**
+ * 通过定时去轮询163的wap站点去判断是否能访问网络
+ */
 ipcMain.on('wwwnet-status-changed', function(event, status) {
-  console.log("收到WWW NET 状态的变化通知 %s",status);
+    console.log("收到WWW NET 状态的变化通知 %s", status);
+    mainWindow.webContents.send('online-status-changed', status)
 });
-
-
--- keepchinanet/renderer.js
+++ keepchinanet/renderer.js
@@ -9,13 +9,23 @@ var $ = jQuery
 
 
 ipcRenderer.on('online-status-changed', function(event, status) {
-  console.log("收到在线/离线状态的变化通知 %s",status);
-  if (status == 'online') {
-  $("#online_span").removeClass("label-default")
-  $("#online_span").addClass("label-success")
-  } else {
-  $("#online_span").removeClass("label-success")
-  $("#online_span").addClass("label-default")
-  }
+    console.log("收到在线/离线状态的变化通知 %s", status);
+    if (status == 'OK') {
+        $("#online_span").removeClass("label-default")
+        $("#online_span").addClass("label-success")
+    } else {
+        $("#online_span").removeClass("label-success")
+        $("#online_span").addClass("label-default")
+    }
 
 });
+
+$("#saveBtn").on("click", function() {
+    console.log("本地保存账号为：%s,%s", $("#chinanet-id").val(), $("#chinanet-pwd").val());
+    window.localStorage.setItem("chinanet-id", $("#chinanet-id").val());
+    window.localStorage.setItem("chinanet-pwd", $("#chinanet-pwd").val());
+})
+
+
+$("#chinanet-id").val(window.localStorage.getItem("chinanet-id"))
+$("#chinanet-pwd").val(window.localStorage.getItem("chinanet-pwd"))
