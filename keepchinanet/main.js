1630ba51d94527af222ab35d640f511dee63cd50
moogle <moogle@live.cn>
2016-08-06 14:49:48 +0800

    增加没10s自动去163服务器检查网络状态的功能

--- keepchinanet/main.js
+++ keepchinanet/main.js
@@ -55,7 +55,7 @@ let onlineStatusWindow;
 app.on('ready', function() {
   console.log("app starting...");
   createWindow();
-  onlineStatusWindow = new BrowserWindow({ width: 400, height: 300, show: false , frame : false, resizable : false});
+  onlineStatusWindow = new BrowserWindow({ width: 400, height: 300, show: true , frame : false, resizable : false});
   if (global.APP_CONFIG.isDevelopment) {
     onlineStatusWindow.webContents.openDevTools()
   }
@@ -87,6 +87,10 @@ app.on('activate', function () {
 ipcMain.on('online-status-changed', function(event, status) {
   console.log("收到在线/离线状态的变化通知 %s",status);
   mainWindow.webContents.send('online-status-changed',status)
+});
+
+ipcMain.on('wwwnet-status-changed', function(event, status) {
+  console.log("收到WWW NET 状态的变化通知 %s",status);
 });
 
 
